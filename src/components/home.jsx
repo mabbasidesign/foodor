a3b50deaf3745d6e3faa3f988742f3d8f85eeace
Mohsen <mabbasidesign@yahoo.com>
2018-09-03 23:40:21 -0600

    Add html-2

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -10,7 +10,14 @@ class Home extends Component {
             <section className="landing">
                 <div className="header">
                     <div className="log"></div>
-                    <div className="navbar"></div>
+                    <div className="navbar">
+                        <ul>
+                            <li>Home</li>
+                            <li>Products</li>
+                            <li>Shope</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
                     <div className="header-cart">
                         <div className="header-log">
                             <ul>
@@ -116,6 +123,7 @@ class Home extends Component {
 
             {/* text service */}
             <section className="service">
+
                 <div className="service-box">
                     <img src="" alt=""/>
                     <div className="service-box-text">
@@ -123,25 +131,118 @@ class Home extends Component {
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque, qui!</p>
                     </div>
                 </div>
+                
             </section>
 
             {/* deal */}
-            <section className="deal"></section>
+            <section className="deal">
+                <img src="" alt=""/>
+                <div className="deal-box">
+                    <h1>BEST DEAL FOR THIS PRODUCT</h1>
+                    <h5>Pineapple juice</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam officia dolores minus!</p>
+                    <a href="#" className="deal-btn">Shop Now</a>
+                </div>
+            </section>
 
             {/* caption */}
-            <section className="seasonal"></section>
+            <section className="seasonal">
+
+                <div className="seasonal-header">
+                    <div className="seasonal-header-box">
+                        <p>Firm-Fressh</p>
+                        <h3>Best Products</h3>
+                    </div>
+
+                </div>
+
+                <div className="seasonal-content">
+
+                    <div className="seasonal-content-box">
+                        <a href="#"><img src="" alt=""/></a>
+                        <div className="seasonal-content-box-text">
+                            <p>Simplement Corn</p>
+                            <span className="seasonal-deal">
+                                <h5>$130.00</h5>
+                                <p>$100.00</p>
+                            </span>
+                            <a href="#">Add to cart</a>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
 
             {/* story */}
-            <section className="story"></section>
+            <section className="story">
+                <img src="" alt=""/>
+                <h2>Tis is a powerfull product online store</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                <p>Lorem ipsum dolor sit amet.</p>
+            </section>
 
             {/* news */}
-            <section className="news"></section>
+            <section className="news">
+                <div className="news-title">
+                    <img src="" alt=""/>
+                    <h2>More Fresh Product</h2>
+                </div>
+                <div className="news-box">
+                    <a href=""><img src="" alt=""/></a>
+                    <p>Lorem, ipsum dolor.</p>
+                    <h5>Lorem ipsum dolor sit amet consectetur.</h5>
+                    <p>Apr 24, 2018 0 comments</p>
+                </div>
+            </section>
 
             {/* newsletter */}
-            <section className="newletter"></section>
+            <section className="newletter">
+                <div className="newsletter-title">
+                    <p>Subscribe</p>
+                    <h3>To Our Newsletter</h3>
+                </div>
+                <div className="newsletter-form">
+                    <form action="">
+                        <input type="text" placehoder="Enter Your E-mail"/>
+                        <a href="#"><button className="newsletter-subscribe" type="submit">Subscribe</button></a>
+                    </form>
+                </div>
+            </section>
 
             {/* footer */}
-            <section className="footer"></section>
+            <section className="footer">
+                <div className="footer-about">
+                    <h6>Fresh Fruit Online Shope</h6>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ipsa!</p>
+                    <h6><span></span> Address </h6>
+                    <h6><span></span> 403-613-6871 </h6>
+                    <h6><span></span> info@example.com</h6>
+                </div>
+                <dv className="footer-option">
+                    <h3>Options</h3>
+                    <ul>
+                        <li>Cart</li>
+                        <li>My Account</li>
+                        <li>Whish List</li>
+                        <li>Login</li>
+                        <li>Register</li>
+                    </ul>
+                </dv>
+                <div className="footer-info">
+                <h3>Information</h3>
+                    <ul>
+                        <li>About</li>
+                        <li>Contact</li>
+                        <li>Privacy Policy</li>
+                        <li>Role</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div className="footer-flicker">
+                    <a href=""><img src="" alt=""/></a>
+                </div>
+            </section>
 
         </body>
         );
